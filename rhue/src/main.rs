cf2f19d77a8a744a1c485cbdb0b756eed9a83fd7
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-01-13 22:55:57 -0500

    Update main.rs
    Some updates on Rhue.

--- rhue/src/main.rs
+++ rhue/src/main.rs
@@ -1,15 +1,23 @@
-use std::{env::args, fs::File, io::Read};
+use std::{env::args, fs::File, io::{Read, self}, result::Result};
 
-fn main() {
-    let args: Vec<String> = args().collect();
-    println!("{}", &args[1]);
-    let file = File::open(&args[1]);
-    let mut buf = String::new();
-    let _ = match file {
-        Ok(mut s_file) => s_file.read_to_string(&mut buf),
-        Err(_) => panic!("goodbye!")
-    };
-    println!("{}", buf);
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = args().skip(1).collect();
+    let rule_file_path = &args[0];
+    let program_file_path = &args[1];
+
+    let mut rule_file = File::open(rule_file_path)?;
+    let mut program_file = File::open(program_file_path)?;
+
+    let mut rule_file_text = String::new();
+    let mut program_file_text = String::new();
+    
+    let _ = rule_file.read_to_string(&mut rule_file_text);
+    let _ = program_file.read_to_string(&mut program_file_text);
+
+    println!("{}", rule_file_text);
+    println!("{}", program_file_text);
+
+    Ok(())
 }
 
 #[test]
